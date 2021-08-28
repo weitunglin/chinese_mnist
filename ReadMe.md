3462bb7e3532fd5cefacc93420495ed977209650
weitunglin <weitung8@gmail.com>
2021-08-28 14:57:55 +0800

    * added confusion matrix * added ReadMe * added dataset into repo * added result

--- ReadMe.md
+++ ReadMe.md
@@ -4,9 +4,33 @@ This project is to training a CNN model
 for [Chinese MNIST Dataset](https://www.kaggle.com/gpreda/chinese-mnist) \
 made by Allen Lin <weitung8@gmail.com>
 
-## Dataset Description
+## Usage
 
-## Network Archtitecture
+### Install Dependencies
+```sh
+pip install -r requirements.txt
+```
+
+### Train the model
+![Usage](usage.png)
+
+### Network Architecture
+![Model](model.png)
+
+
+## Accuracy
+Training accuracy: 99.3%
+Testing accurarcy: 99.8%
+
+### Tensorboard
+```sh
+tensorboard --logdir runs
+```
+
+open [Tensorboard](http://localhost:6006) to see complete result
+
+### Confusion Matrix
+![ConfusionMatrix](confusion_matrix.png)
 
 ## References
 * https://www.kaggle.com/ricafernandes/chinese-mnist-pytorch-cnn-92-training
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import argparse
 import torch
 import torch.nn as nn
@@ -8,9 +7,13 @@ from torch.utils.data.dataset import random_split
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torchsummary import summary
-import pandas as pd
+from torchvision.utils import make_grid
 from torch.utils.tensorboard import SummaryWriter
 from dataset import ChineseMNISTDataset
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 class Net(nn.Module):
     def __init__(self):
@@ -23,7 +26,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(2048, 512)
         self.fc2 = nn.Linear(512, 128)
         self.fc3 = nn.Linear(128, 15)
-        self.softmax = nn.LogSoftmax(dim=0)
+        # self.softmax = nn.LogSoftmax(dim=0)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -33,7 +36,7 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        x = self.softmax(x)
+        # x = self.softmax(x)
         # print(x.shape)
         return x
 
@@ -45,16 +48,10 @@ def train(model, criterion, optimizer, train_loader, epoch, tensor_board):
 
     model.train()
     for i, (images, codes) in enumerate(train_loader):
-        # print(i)
-        # print(len(images), images.shape, len(codes), codes.shape)
-        # print(codes)
-
         optimizer.zero_grad()
-
         outputs = model(images)
-        # print(outputs, codes)
+
         loss = criterion(outputs, codes)
-        # print(loss)
         loss.backward()
         optimizer.step()
 
@@ -64,15 +61,14 @@ def train(model, criterion, optimizer, train_loader, epoch, tensor_board):
 
         running_loss += loss.item()
         if i % 20 == 19:
-            print(f"loss: {running_loss/20:>.3f} [{i * len(images)}/{len(train_loader)*len(images)}]")
-
-            tensor_board.add_scalar(f"Epoch {epoch} Training Loss (by batch)", running_loss/20, i)
+            print(f"Loss: {running_loss / 20:>.3f} [{(i + 1) * len(images)}/{len(train_loader) * len(images)}]")
+            tensor_board.add_scalar(f"Epoch {epoch} Training Loss (by batch)", running_loss / 20, i)
             running_loss = 0
-    
+ 
     total_count = labels.size(0)
     correct_count = (labels == preds).sum().item()
     tensor_board.add_scalar(f"Training Accuracy (by epoch)", correct_count / total_count, epoch)
-    print(f'Training Accuracy: {correct_count/total_count}\n')
+    print(f"Training Accuracy: {correct_count / total_count}")
 
 def test(model, test_loader, epoch, tensor_board):
     labels = torch.tensor([]).detach()
@@ -88,8 +84,8 @@ def test(model, test_loader, epoch, tensor_board):
     
     total_count = labels.size(0)
     correct_count = (labels == preds).sum().item()
-    tensor_board.add_scalar(f"Testing Accuracy (by epoch)", correct_count / total_count, epoch)
-    print(f'Testing Accuracy: {correct_count/total_count}\n')
+    tensor_board.add_scalar(f"Validation Accuracy (by epoch)", correct_count / total_count, epoch)
+    print(f"Validation Accuracy: {correct_count / total_count}")
 
 def main():
     parser = argparse.ArgumentParser(description="Chinese MNIST simple CNN")
@@ -98,18 +94,25 @@ def main():
     parser.add_argument("--epochs", type=int, default=3, metavar="N", help="number of epochs to train (default: 3)")
     parser.add_argument("--data-file-path", type=str, default="/Users/allen/ml/chinese_mnist/chinese_mnist.csv", help="path to the csv data file")
     parser.add_argument("--img-folder-path", type=str, default="/Users/allen/ml/chinese_mnist/img", help="path to the csv data file")
+    parser.add_argument("--run-name", type=str, default=datetime.now().strftime("%Y/%m/%d %H:%M:%S"), help="run id for tensor board")
+    parser.add_argument("--random-seed", type=int, default=42, help="random seed")
 
     args = parser.parse_args()
-    print(f"args: {args}")
+    print("----- args -----")
+    print(args)
 
     data = pd.read_csv(args.data_file_path, low_memory=False)
     data["img_path"] = data.apply(lambda row: f"{args.img_folder_path}/input_{row.suite_id}_{row.sample_id}_{row.code}.jpg", axis=1)
+    print("----- dataset (head & describe) -----")
     print(data.head())
+    print(data.describe())
 
-    train_dataset = data.sample(frac=0.8, random_state=42)
+    train_dataset = data.sample(frac=0.8, random_state=args.random_seed)
     test_dataset = data.drop(train_dataset.index)
 
-    transform = transforms.Compose([transforms.ToTensor()])
+    transform = transforms.Compose([
+        transforms.RandomRotation(10),
+        transforms.ToTensor()])
 
     train_loader = DataLoader(ChineseMNISTDataset(train_dataset, args.img_folder_path, transform), batch_size=args.batch_size, shuffle=True)
     test_loader = DataLoader(ChineseMNISTDataset(test_dataset, args.img_folder_path, transform), batch_size=args.test_batch_size, shuffle=True)
@@ -120,16 +123,56 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    tensor_board = SummaryWriter()
+    tensor_board = SummaryWriter(f"runs/{args.run_name}")
+    sample_images = iter(test_loader).next()[0]
+    tensor_board.add_image("chinese mnist", make_grid(sample_images))
+    tensor_board.add_graph(model, sample_images)
 
-    for epoch in range(args.epochs):
-        print(f"Epoch {epoch + 1}\n-----------------------")
+    for epoch in range(1, args.epochs + 1):
+        print(f"----- Epoch {epoch} -----")
         train(model, criterion, optimizer, train_loader, epoch, tensor_board)
         test(model, test_loader, epoch, tensor_board)
+        print(f"----- End of Epoch {epoch} -----")
 
-    MODEL_PATH = './chinese_mnist.pth'
-    torch.save(model.state_dict(), MODEL_PATH)
+    model.eval()
+    dataset = DataLoader(ChineseMNISTDataset(data, args.img_folder_path, transform), batch_size=args.batch_size) 
 
+    classes = ("零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "百", "千", "萬", "億")
+    class_probs = []
+    class_label = []
+    with torch.no_grad():
+        for data in dataset:
+            images, labels = data
+            output = model(images)
+            class_probs_batch = [F.softmax(el, dim=0) for el in output]
+
+            class_probs.append(class_probs_batch)
+            class_label.append(labels)
+
+    test_probs = torch.cat([torch.stack(batch) for batch in class_probs])
+    test_label = torch.cat(class_label)
+
+    def add_pr_curve_tensorboard(class_index, test_probs, test_label, global_step=0):
+        tensorboard_truth = test_label == class_index
+        tensorboard_probs = test_probs[:, class_index]
+
+        tensor_board.add_pr_curve(classes[class_index],
+                            tensorboard_truth,
+                            tensorboard_probs,
+                            global_step=global_step)
+
+    for i in range(len(classes)):
+        add_pr_curve_tensorboard(i, test_probs, test_label)
+
+    test_preds = np.array([], dtype=int)
+    for batch in class_probs:
+        for i in batch: 
+            test_preds = np.append(test_preds, torch.argmax(i).unsqueeze(-1))
+    df_confusion = pd.crosstab(test_label, test_preds, rownames=["Actual"], colnames=["Predicted"], margins=True)
+    print("----- Confusion Matrix -----")
+    print(df_confusion)
+
+    torch.save(model.state_dict(), f"result/{args.run_name}.pth")
     tensor_board.close()
 
 if __name__ == "__main__":
